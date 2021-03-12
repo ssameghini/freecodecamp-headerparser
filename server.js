3955a334e7e770448d023b7b89f7d7a091cdeb1c
Sebastian Sameghini <s.sameghini@gmail.com>
2021-03-12 20:56:32 -0300

    API done

--- server.js
+++ server.js
@@ -19,13 +19,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// My tested API
+app.get('/api/whoami', function(req, res) {
+  var language = req.headers['accept-language'];
+  var userAgent = req.headers['user-agent'];
+  var reqIP = req.ip;
+  res.json({ "ipadress":reqIP, "language":language, "software":userAgent });
+});
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
